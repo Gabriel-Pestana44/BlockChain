aad6e7bf69436fd29e1cccd6ce70e539ccc38530
Gabriel Pestana <gabriel99094@gmail.com>
2026-03-10 20:22:22 -0300

    att: data retention via JSON

--- bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Compilando..."
-javac -cp "../lib/gson-2.11.0.jar:." BlockChain/*.java App.java
-if [ $? -eq 0 ]; then
-    echo "Rodando..."
-    java -cp "../lib/gson-2.11.0.jar:." App
-else
-    echo "Erro na compilação!"
-fi
